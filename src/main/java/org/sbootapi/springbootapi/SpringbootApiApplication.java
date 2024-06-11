1efbc0257e48c26da0ce642a460994c5221d1c7c
VHSug <vitorhidekisuga@hotmail.com>
2024-06-11 15:27:23 -0300

    Entidade User e REST

--- src/main/java/org/sbootapi/springbootapi/SpringbootApiApplication.java
+++ src/main/java/org/sbootapi/springbootapi/SpringbootApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootApiApplication {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         SpringApplication.run(SpringbootApiApplication.class, args);
     }
 
